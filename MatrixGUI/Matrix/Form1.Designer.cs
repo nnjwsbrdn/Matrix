80217673ddac9a37d288aed106b874173342a36a
Gard <116628000+4Gard@users.noreply.github.com>
2022-12-16 12:00:40 +0800

    Add files via upload
    Matrix 3by3 and 2by2, (.exe file in bin/debug/Matrix.exe)

---
+++ MatrixGUI/Matrix/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Matrix
+{
+    partial class Matrix
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Matrix3x3 = new System.Windows.Forms.TabControl();
+            this.Matrix3by3 = new System.Windows.Forms.TabPage();
+            this.Matrix2by2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.Matrix3x3.SuspendLayout();
+            this.Matrix3by3.SuspendLayout();
+            this.Matrix2by2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Matrix3x3
+            // 
+            this.Matrix3x3.Controls.Add(this.Matrix3by3);
+            this.Matrix3x3.Controls.Add(this.Matrix2by2);
+            this.Matrix3x3.Location = new System.Drawing.Point(2, 3);
+            this.Matrix3x3.Name = "Matrix3x3";
+            this.Matrix3x3.SelectedIndex = 0;
+            this.Matrix3x3.Size = new System.Drawing.Size(786, 435);
+            this.Matrix3x3.TabIndex = 0;
+            // 
+            // Matrix3by3
+            // 
+            this.Matrix3by3.Controls.Add(this.textBox13);
+            this.Matrix3by3.Controls.Add(this.textBox12);
+            this.Matrix3by3.Controls.Add(this.textBox11);
+            this.Matrix3by3.Controls.Add(this.textBox10);
+            this.Matrix3by3.Controls.Add(this.textBox9);
+            this.Matrix3by3.Controls.Add(this.textBox8);
+            this.Matrix3by3.Controls.Add(this.textBox7);
+            this.Matrix3by3.Controls.Add(this.textBox6);
+            this.Matrix3by3.Controls.Add(this.textBox5);
+            this.Matrix3by3.Location = new System.Drawing.Point(4, 24);
+            this.Matrix3by3.Name = "Matrix3by3";
+            this.Matrix3by3.Padding = new System.Windows.Forms.Padding(3);
+            this.Matrix3by3.Size = new System.Drawing.Size(778, 407);
+            this.Matrix3by3.TabIndex = 0;
+            this.Matrix3by3.Text = "Matrix3by3";
+            this.Matrix3by3.UseVisualStyleBackColor = true;
+            // 
+            // Matrix2by2
+            // 
+            this.Matrix2by2.Controls.Add(this.textBox4);
+            this.Matrix2by2.Controls.Add(this.textBox3);
+            this.Matrix2by2.Controls.Add(this.textBox2);
+            this.Matrix2by2.Controls.Add(this.textBox1);
+            this.Matrix2by2.Location = new System.Drawing.Point(4, 24);
+            this.Matrix2by2.Name = "Matrix2by2";
+            this.Matrix2by2.Padding = new System.Windows.Forms.Padding(3);
+            this.Matrix2by2.Size = new System.Drawing.Size(778, 407);
+            this.Matrix2by2.TabIndex = 1;
+            this.Matrix2by2.Text = "Matrix2by2";
+            this.Matrix2by2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(45, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(97, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(45, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(46, 108);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(45, 23);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(97, 108);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(45, 23);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(38, 54);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(45, 23);
+            this.textBox5.TabIndex = 0;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(89, 54);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(45, 23);
+            this.textBox6.TabIndex = 1;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(140, 54);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(45, 23);
+            this.textBox7.TabIndex = 2;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(38, 83);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(45, 23);
+            this.textBox8.TabIndex = 3;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(89, 83);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(45, 23);
+            this.textBox9.TabIndex = 4;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(140, 83);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(45, 23);
+            this.textBox10.TabIndex = 5;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(38, 112);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(45, 23);
+            this.textBox11.TabIndex = 6;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(89, 112);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(45, 23);
+            this.textBox12.TabIndex = 7;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(140, 112);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(45, 23);
+            this.textBox13.TabIndex = 8;
+            // 
+            // Matrix
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Matrix3x3);
+            this.Name = "Matrix";
+            this.Text = "Matrix";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Matrix3x3.ResumeLayout(false);
+            this.Matrix3by3.ResumeLayout(false);
+            this.Matrix3by3.PerformLayout();
+            this.Matrix2by2.ResumeLayout(false);
+            this.Matrix2by2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TabControl Matrix3x3;
+        private TabPage Matrix3by3;
+        private TextBox textBox13;
+        private TextBox textBox12;
+        private TextBox textBox11;
+        private TextBox textBox10;
+        private TextBox textBox9;
+        private TextBox textBox8;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TabPage Matrix2by2;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+    }
+}
---
+++ MatrixGUI/Matrix/Form1.cs
@@ -0,0 +1,15 @@
+namespace Matrix
+{
+    public partial class Matrix : Form
+    {
+        public Matrix()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
